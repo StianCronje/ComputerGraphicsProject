43bbc11040d483cc4048a0ceb0be930249e0ade1
Virgil Martinez <virgilmartinez@gmail.com>
2018-04-30 17:51:55 -0500

    Added a 'star background'
    Its a cylinder drawn 90 degrees rotated. It has no top or bottom (I think a bottom would ruin the illusion?

---
+++ main.cpp
@@ -0,0 +1,243 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "Detect.h"
+#include "Window.h"
+#include "Model.h"
+#include "ObstacleSpawner.h"
+#include "PerlinGen.h"
+#include "common/controls.hpp"
+
+#include <chrono>
+
+//void process_lighting();
+void reset(Model* player, std::vector<ObstacleSpawner>& spawner);
+void crash(Model* player);
+
+glm::vec3 ambientColor = glm::vec3(0.2, 0.2, 0.1);
+glm::vec3 diffusePosition = glm::vec3(20.0, 50.0, 0.0);
+glm::vec3 diffuseColor = glm::vec3(1.0, 1.0, 1.0);
+glm::vec3 cameraLightPos = glm::vec3(0, 0, 0);
+glm::vec3 cameraLightColor = glm::vec3(0.4, 0.6, 1.0);
+glm::vec3 explosionLightPos = glm::vec3(0, 0, 0);
+glm::vec3 explosionLightColor = glm::vec3(1.0, 0.6, 0.0);
+glm::vec3 specColor = glm::vec3(0.2, 0.2, 0.1);
+
+glm::vec3 crashPos = glm::vec3(0, 0, 0);
+glm::vec3 startExplosionLightPos = glm::vec3(0, 0, 0);
+glm::vec3 offExplosionLightColor = glm::vec3(0, 0, 0);
+glm::vec3 onExplosionLightColor = glm::vec3(1.0, 0.6, 0.0);
+
+std::vector<Model*> astroids;
+
+float a = 0.0, b = 0.0, c = 0.0;
+glm::vec3 cameraOffset = glm::vec3(0, 0, 20);
+glm::vec3 cameraLookat = glm::vec3(0, 0, -2);
+float offsetDamp = 0.1f;
+float lookatDamp = 0.2f;
+float startShipTurnSpeed = 0.5;
+float startShipTurnAngle = 25;
+float startShipMoveSpeed = 0.1;
+float shipTurnSpeed;
+float shipMoveSpeed;
+float shipTurnAngle;
+
+int spawn_count_x = 10;
+int spawn_count_y = 10;
+int x_size = 200;
+int y_size = 50;
+int z_size = 100;
+int blocksPassed = 0;
+int currentBlockIndex = 0;
+
+double frameStart = 0.0;
+double frameEnd = 0.0;
+double deltaTime = 1.0;
+
+glm::vec3 tempVec3(0, 0, 0);
+
+int main() {
+
+	Window gameWindow("Astroid Field", 1024, 768);
+	glClearColor(0.01f, 0.0f, 0.03f, 1.0f);
+	std::cout << "OpenGL" << glGetString(GL_VERSION) << std::endl;
+
+
+	Model::ambientColor = ambientColor;
+	Model::diffusePosition = diffusePosition;
+	Model::diffuseColor = diffuseColor;
+	Model::cameraLightPos = cameraLightPos;
+	Model::cameraLightColor = cameraLightColor;
+	Model::explosionLightPos = explosionLightPos;
+	Model::explosionLightColor = explosionLightColor;
+
+	setCameraSpeed(0);
+
+
+	Model playerShip(gameWindow.getWindow(), "Models/Ship_3.obj", "Models/Ship_tex.png");
+	Model astrd1(gameWindow.getWindow(), "Models/astrd_1.obj", "Models/FireAsteroid.jpg");
+	Model astrd2(gameWindow.getWindow(), "Models/astrd_2.obj", "Models/Asteroid5.png");
+	Model astrd3(gameWindow.getWindow(), "Models/astrd_3.obj", "Models/BlueAsteroid.jpg");
+	Model astrd4(gameWindow.getWindow(), "Models/astrd_4.obj", "Models/Asteroid8.jpg");
+	Model background(gameWindow.getWindow(), "Models/background.obj", "Models/background.jpg");
+	astroids.push_back(&astrd1);
+	astroids.push_back(&astrd2);
+	astroids.push_back(&astrd3);
+	astroids.push_back(&astrd4);
+
+	x_size /= spawn_count_x;
+	y_size /= spawn_count_x;
+	z_size /= spawn_count_y;
+
+	std::vector<ObstacleSpawner> obstaclesArray{
+		ObstacleSpawner(astroids),
+		ObstacleSpawner(astroids),
+		ObstacleSpawner(astroids)
+	};
+	currentBlockIndex = blocksPassed % obstaclesArray.size();
+
+	reset(&playerShip, obstaclesArray);
+
+	cameraOffset.y *= y_size;
+	setCameraPosition(cameraOffset);
+	setCameraLookat(cameraLookat);
+
+	while (glfwGetKey(gameWindow.getWindow(), GLFW_KEY_ESCAPE) != GLFW_PRESS && !gameWindow.closed())
+	{
+		gameWindow.clear();
+		frameStart = glfwGetTime();
+
+
+		//=== Loop Here ===
+
+		// example on how to get a key input
+		playerShip.SetRotation(glm::vec3(0.0f, 0.0f, 0.0f));
+
+		c += shipMoveSpeed;
+
+		if (gameWindow.isKeyPressed(GLFW_KEY_R))
+			reset(&playerShip, obstaclesArray);
+
+		if (gameWindow.isKeyPressed(GLFW_KEY_A))
+		{
+			std::cout << "'A' is pressed" << std::endl;
+			if (b >= -47) {
+				b -= shipTurnSpeed;
+				playerShip.SetRotation(glm::vec3(0.0f, 0.0f, shipTurnAngle));
+			}
+		}
+		if (gameWindow.isKeyPressed(GLFW_KEY_S))
+		{
+			std::cout << "'S' is pressed" << std::endl;
+			if (a <= 55) {
+				a += shipTurnSpeed;
+				playerShip.SetRotation(glm::vec3(shipTurnAngle, 0.0f, 0.0f));
+			}
+		}
+		if (gameWindow.isKeyPressed(GLFW_KEY_W))
+		{
+
+			std::cout << "'W' is pressed" << std::endl;
+			if (a >= -17) {
+				a -= shipTurnSpeed;
+				playerShip.SetRotation(glm::vec3(-shipTurnAngle, 0.0f, 0.0f));
+			}
+		}
+		if (gameWindow.isKeyPressed(GLFW_KEY_D))
+		{
+			std::cout << "'D' is pressed" << std::endl;
+			if (b <= 47) {
+				b += shipTurnSpeed;
+				playerShip.SetRotation(glm::vec3(0.0f, 0.0f, -shipTurnAngle));
+			}
+		}
+
+		// example on how to get mouse buttons
+		if (gameWindow.isMouseButtonPressed(GLFW_MOUSE_BUTTON_1))
+		{
+			std::cout << "camspeed: " << getCameraSpeed() << std::endl;
+			shipMoveSpeed += 0.1;
+		}
+
+		playerShip.SetScale(glm::vec3(2, 2, 2));
+		playerShip.SetTranslation(glm::vec3(b, a, -c));
+		background.SetScale(glm::vec3(500, 500, 9999));
+		background.SetTranslation(glm::vec3(0, 0, 0));
+		background.SetRotation(glm::vec3(90, 0, 0));
+		//tempVec3 = getCameraPosition();
+		tempVec3 = playerShip.GetTranslation() + cameraOffset;
+		tempVec3.y *= offsetDamp;
+		tempVec3.x *= offsetDamp;
+		setCameraPosition(tempVec3);
+		tempVec3 = playerShip.GetTranslation() + cameraLookat;
+		tempVec3.y *= lookatDamp;
+		tempVec3.x *= lookatDamp;
+		setCameraLookat(tempVec3);
+		cameraLightPos = getCameraPosition();
+		background.Draw();
+		playerShip.Draw();
+
+		for (unsigned int i = 0; i < obstaclesArray.size(); i++)
+		{
+			obstaclesArray[i].Spawn();
+			if (obstaclesArray[i].CheckColission(&playerShip, 10))
+			{
+				std::cout << "HIT ASTROID" << std::endl;
+				crash(&playerShip);
+			}
+		}
+
+		// if the camera is at the end of the block
+		if (getCameraPosition().z <= obstaclesArray[currentBlockIndex].GetOffset().z * spawn_count_y)
+		{
+			glm::vec3 temp = obstaclesArray[currentBlockIndex].GetOffset();
+			temp.z -= obstaclesArray.size() * z_size;
+			obstaclesArray[currentBlockIndex].SetOffset(temp);
+			obstaclesArray[currentBlockIndex].Generate(spawn_count_x, spawn_count_y, x_size, y_size, z_size);
+
+			blocksPassed++;
+			currentBlockIndex = blocksPassed % obstaclesArray.size();
+		}
+
+		std::cout << "FPS: " << static_cast<int>(1 / deltaTime) << std::endl;
+
+		//=== End Loop ===
+		frameEnd = glfwGetTime();
+		deltaTime = frameEnd - frameStart;
+		gameWindow.update();
+	}
+
+	return 0;
+}
+
+void reset(Model* player, std::vector<ObstacleSpawner>& spawner)
+{
+	// reset player pos and input
+	a = b = c = 0.0;
+	player->SetTranslation(glm::vec3(b, a, -c));
+	shipTurnSpeed = startShipTurnSpeed;
+	shipMoveSpeed = startShipMoveSpeed;
+	shipTurnAngle = startShipTurnAngle;
+
+	//reset crash light
+	Model::explosionLightColor = offExplosionLightColor;
+	Model::explosionLightPos = startExplosionLightPos;
+
+	// reset obstacle pos
+	for (int i = 0; i < spawner.size(); i++)
+	{
+		spawner[i].SetOffset(glm::vec3(-x_size / 4, 0, -z_size * (i + 1)));
+		spawner[i].Generate(spawn_count_x, spawn_count_y, x_size, y_size, z_size);
+		std::cout << "Spawn Block at Z: " << spawner[i].GetOffset().z << std::endl;
+	}
+
+}
+
+void crash(Model* player)
+{
+	Model::explosionLightPos =  player->GetTranslation() + glm::vec3(0, 1, 0);
+	Model::explosionLightColor = onExplosionLightColor * glm::vec3(2, 2, 2);
+	shipMoveSpeed = 0;
+	shipTurnSpeed = 0;
+	shipTurnAngle = 0;
+}
